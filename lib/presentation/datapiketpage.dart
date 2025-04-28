c8de67f33cb6c4d2579a6a49ae4df22bebf74659
Alan21103 <syahlansaputra03@gmail.com>
2025-04-29 00:16:09 +0700

    Membuat form data piket dengan date picker dan tampilan berwarna ungu

---
+++ lib/presentation/datapiketpage.dart
@@ -0,0 +1,325 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ucp_1/presentation/detaildatapiket.dart';
+
+class DataPiketPage extends StatefulWidget {
+  const DataPiketPage({super.key});
+
+  @override
+  State<DataPiketPage> createState() => _DataPiketPageState();
+}
+
+class _DataPiketPageState extends State<DataPiketPage> {
+  final _key = GlobalKey<FormState>();
+  final TextEditingController _tugasController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+
+  List<Map<String, dynamic>> listTugas = [];
+  String? _selectedAnggota;
+  String? _dateError;
+  List<String> _anggotaOptions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadEmail();
+  }
+
+  void _loadEmail() async {
+    final prefs = await SharedPreferences.getInstance();
+    final email = prefs.getString('userEmail') ?? 'Admin';
+    setState(() {
+      _anggotaOptions = [email];
+      _selectedAnggota = email;
+    });
+  }
+
+  @override
+  void dispose() {
+    _tugasController.dispose();
+    _dateController.dispose();
+    super.dispose();
+  }
+
+  void addTugas() {
+    setState(() {
+      _dateError = _dateController.text.isEmpty ? 'Harap pilih tanggal' : null;
+    });
+
+    if (_key.currentState!.validate() && _dateError == null) {
+      setState(() {
+        listTugas.add({
+          'anggota': _selectedAnggota,
+          'tanggal': _dateController.text,
+          'tugas': _tugasController.text,
+        });
+        _tugasController.clear();
+        _dateController.clear();
+        _dateError = null;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Tugas berhasil ditambahkan!'),
+          backgroundColor: Colors.green,
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        _dateController.text = DateFormat('dd-MM-yyyy').format(pickedDate);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Piket Gudang',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: const Color.fromARGB(255, 134, 11, 156),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Nama Anggota Section
+            Text(
+              'Nama Anggota',
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            DropdownButtonFormField<String>(
+              value: _selectedAnggota,
+              items:
+                  _anggotaOptions.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedAnggota = newValue;
+                });
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Pilih Tanggal Section
+            Text(
+              'Pilih Tanggal',
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _dateController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      hintText: 'Pilih Tanggal',
+                      hintStyle: GoogleFonts.poppins(),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.calendar_today),
+                        onPressed: () => _selectDate(context),
+                      ),
+                    ),
+                    onTap: () => _selectDate(context),
+                  ),
+                ),
+              ],
+            ),
+            if (_dateError != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 4),
+                child: Text(
+                  _dateError!,
+                  style: GoogleFonts.poppins(color: Colors.red, fontSize: 12),
+                ),
+              ),
+            const SizedBox(height: 16),
+
+            // Tugas Piket Section
+            Text(
+              'Tugas Piket',
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: Form(
+                    key: _key,
+                    child: TextFormField(
+                      controller: _tugasController,
+                      decoration: InputDecoration(
+                        hintText: 'Tugas Piket',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) return 'Masukkan tugas piket';
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: addTugas,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 134, 11, 156),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    'Tambah',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+
+            // Daftar Tugas Piket Section
+            Text(
+              'Daftar Tugas Piket',
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child:
+                  listTugas.isEmpty
+                      ? Center(
+                        child: Text(
+                          'Belum ada Data',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      )
+                      : ListView.builder(
+                        itemCount: listTugas.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            margin: const EdgeInsets.symmetric(vertical: 8),
+                            child: ListTile(
+                              title: Text(
+                                listTugas[index]['tugas'],
+                                style: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      Colors
+                                          .purple, 
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder:
+                                        (context) => DetailDataPiketPage(
+                                          tugas: listTugas[index]['tugas'],
+                                          anggota: listTugas[index]['anggota'],
+                                          tanggal: listTugas[index]['tanggal'],
+                                        ),
+                                  ),
+                                );
+                              },
+                              trailing: IconButton(
+                                icon: const Icon(
+                                  Icons
+                                      .arrow_forward, 
+                                  color:
+                                      Colors
+                                          .purple, 
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder:
+                                          (context) => DetailDataPiketPage(
+                                            tugas: listTugas[index]['tugas'],
+                                            anggota:
+                                                listTugas[index]['anggota'],
+                                            tanggal:
+                                                listTugas[index]['tanggal'],
+                                          ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

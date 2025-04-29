d3f157b3315377b5461c855bb0e5a880579770e9
Alan21103 <syahlansaputra03@gmail.com>
2025-04-29 18:10:50 +0700

    Mengimport semua dan membuat navigasi seluruh halaman di main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:ucp_1/presentation/databarangpage.dart';
+import 'package:ucp_1/presentation/datapelanggan.dart';
+import 'package:ucp_1/presentation/datapiketpage.dart';
+import 'package:ucp_1/presentation/homepage.dart';
+import 'package:ucp_1/presentation/loginpage.dart';
+import 'package:ucp_1/presentation/registerpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,24 +19,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/datapiket': (context) => const DataPiketPage(),
+        '/datapelanggan': (context) => const DataPelangganPage(),
+        '/databarang': (context) => const PendataanBarangPage(),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
